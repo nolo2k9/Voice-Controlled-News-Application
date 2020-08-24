e0802b17ce2f4c1da0750dda7b09a34b616d1a1a
Keith Nolan <g00351932@gmit.ie>
2020-08-24 16:44:14 +0100

    Getting voice assistant set up

--- alan-ai-news-application/src/app.js
+++ alan-ai-news-application/src/app.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
+const alanKey = '2ddc25d3e070a5fec68dcf2f84260ed32e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+    /*
+        useEffect is ran once the project is mounted 
+        Run once or multiple times
+    */
+    useEffect(() => {
+        //setting up what alan needs to run
+        alanBtn({
+            key: alanKey,
+            onCommand : ({command, articles}) => {
+                if(command === 'newHeadlines'){
+                    console.log(articles);
+                }
+            }
+
+        })
+    }, [])
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
