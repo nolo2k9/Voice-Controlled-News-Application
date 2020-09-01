47636978816a71515b02ad10d4080041ebe2737d
Keith Nolan <g00351932@gmit.ie>
2020-09-01 17:38:25 +0100

    Added cards for different news stories with images and defaultimage

--- alan-ai-news-application/src/components/NewsCard/NewsCard.js
+++ alan-ai-news-application/src/components/NewsCard/NewsCard.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import Newscards from '../NewsCards/NewsCards';
-
-const Newscard = (props) => {
+import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import useStyles from './style.js';
+/*
+De-structure properties coming from article
+*/
+const NewsCard = ({article: {description, publishedAt, source, title, url, urlToImage}, i}) => {
+    const classes = useStyles();
+    
     return (
-        <div>
-            News Card
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia className ={classes.media}image={urlToImage ||'https://pixabay.com/illustrations/news-headlines-newsletter-636978/'}/>
+                <div>
+    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography gutterBottom varient="h5">{title}</Typography>
+                <CardContent>
+                    <Typography varient="body2" color="textSecondary" component ="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="smaill" color="primary">Learn More</Button>
+                <Typography varient="h5" color="textSecondary">{i + 1}</Typography>
+
+               
+            </CardActions>
+        </Card>
     )
 }
 
-export default Newscard;
+export default NewsCard;
