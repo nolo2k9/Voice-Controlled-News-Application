2de860d63ed279067abbb937e036d82a4371d90c
Keith Nolan <g00351932@gmit.ie>
2020-09-23 11:48:16 +0100

    Added some styles to news cards

--- alan-ai-news-application/src/components/NewsCard/NewsCard.js
+++ alan-ai-news-application/src/components/NewsCard/NewsCard.js
@@ -8,10 +8,10 @@ const NewsCard = ({article: {description, publishedAt, source, title, url, urlTo
     const classes = useStyles();
     
     return (
-        <Card>
-            <CardActionArea>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
                 <CardMedia className ={classes.media}image={urlToImage ||'https://pixabay.com/illustrations/news-headlines-newsletter-636978/'}/>
-                <div>
+                <div className={classes.details}>
     <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
                     <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                 </div>
@@ -20,7 +20,7 @@ const NewsCard = ({article: {description, publishedAt, source, title, url, urlTo
                     <Typography varient="body2" color="textSecondary" component ="p">{description}</Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size="smaill" color="primary">Learn More</Button>
                 <Typography varient="h5" color="textSecondary">{i + 1}</Typography>
 
